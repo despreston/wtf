1ccf200363953310b883e77405ae057de2ac9f85
Des Preston <despreston@gmail.com>
2022-04-15 15:45:26 -0400

    feat(steam): adding steam status module

---
+++ modules/steam/keyboard.go
@@ -0,0 +1,17 @@
+package steam
+
+import "github.com/gdamore/tcell/v2"
+
+func (widget *Widget) initializeKeyboardControls() {
+	widget.InitializeHelpTextKeyboardControl(widget.ShowHelp)
+	widget.InitializeRefreshKeyboardControl(widget.Refresh)
+
+	widget.SetKeyboardChar("j", widget.Next, "Select next item")
+	widget.SetKeyboardChar("k", widget.Prev, "Select previous item")
+	// widget.SetKeyboardChar("o", widget.openSteam, "Open target URL in browser")
+
+	widget.SetKeyboardKey(tcell.KeyDown, widget.Next, "Select next item")
+	widget.SetKeyboardKey(tcell.KeyUp, widget.Prev, "Select previous item")
+	// widget.SetKeyboardKey(tcell.KeyEnter, widget.openTwitch, "Open stream in browser")
+	widget.SetKeyboardKey(tcell.KeyEsc, widget.Unselect, "Clear selection")
+}
